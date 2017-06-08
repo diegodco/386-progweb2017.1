ff6afc3f68e438aef9f7409b15433d4d167a9a98
Diêgo Oliveira <universodco@gmail.com>
2017-06-08 11:59:32 -0300

    Questão 3, Lista II

--- Lista_II/Questao_3.py
+++ Lista_II/Questao_3.py
@@ -6,4 +6,14 @@ faça um programa que leia a variável peso (peso de peixes) e verifique se há 
 variável excesso e na variável multa o valor da multa que João deverá pagar. Caso contrário mostrar tais
 variáveis com o conteúdo ZERO.
 '''
-
+peso = float(input('Digite o peso de Peixes (Kg): '))
+if peso <= 50:
+    print('\nPeso de Peixes: %.2f Kg\n'%peso)
+    print('Você não excedeu o limite de pesca!')
+else:
+    print('\nPeso de Peixes: %.2f Kg\n' % peso)
+    print('Você excedeu o limite de pesca!\n')
+    excesso = peso - 50
+    multa = excesso * 4
+    print('Exesso de peso: %.2f Kg'%excesso)
+    print('Valor da multa: R$ %.2f' %multa)
